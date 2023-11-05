c6ed0193d45262ba75edefa2d73036ef1adc18d5
Saif <saifisami2005@gmail.com>
2023-11-05 03:11:17 -0500

    Finished the exercise.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Let's get this party started!");
+$("form").on("click", "#search", search);
+$("form").on("click", "#remove", removeImages);
+$("form").on("submit", search);
+
+async function search(e) {
+    e.preventDefault();
+    const response = await axios.get("https://api.giphy.com/v1/gifs/search", { params: { api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", q: $("form input").val() } });
+    const newGif = $(`<img class="gif">`).attr("src", response.data.data[0].images.original.url);
+    $("#gif-container").append(newGif);
+    $("form input").val("");
+};
+
+function removeImages() { $("img").remove() };
